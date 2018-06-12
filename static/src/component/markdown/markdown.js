7050c15c9d94496579de07e54347f1e9323125b5
jochenshi <jochenshi@gmail.com>
2018-06-12 16:25:53 +0800

    add tab keyboard event in markdown and add save keyboard detect function

--- static/src/component/markdown/markdown.js
+++ static/src/component/markdown/markdown.js
@@ -54,12 +54,23 @@ class Markdown extends Component{
         }, 2000)
     };
 
+    //处理按键按下的事件，在此处是为了实现Tab缩进的效果
     handleKeyDown = (e) => {
         e.persist();
-        console.log(e);
         if (e.keyCode === 9) {
+            let target = e.target;
+            let nowPosition = target.selectionStart + 4;
+            target.value = target.value.substr(0, target.selectionStart) + "    " + target.value.substr(target.selectionStart);
+            target.selectionStart = nowPosition;
+            target.selectionEnd = nowPosition;
+            target.focus();
             e.preventDefault();
-            console.log(e.keyCode)
+        } else if (e.keyCode === 83) {
+            //this represent press "ctrl/command" + "S", and "ctrl/command" is pressed before "S"
+            if (e.metaKey || e.ctrlKey) {
+                console.log("execute save operation");
+                e.preventDefault()
+            }
         }
     };
 
@@ -67,7 +78,7 @@ class Markdown extends Component{
         return (
             <div className={"markdown-wrapper"}>
                 <div className={"markdown-area"}>
-                    <input className={"markdown-title"} onChange={this.handleInput}/>
+                    <input className={"markdown-title"} onChange={this.handleInput} placeholder={"请输入标题"}/>
                     <ul className={"markdown-operation"}>
                         {
                             this.icons.length ?
@@ -80,7 +91,7 @@ class Markdown extends Component{
                     <textarea
                         className={"markdown-input"}
                         onKeyDown={this.handleKeyDown}
-                        onChange={this.handleMark}></textarea>
+                        onChange={this.handleMark}/>
                 </div>
                 <div className={"display-area"}>
                     <h1 className={"display-title"}>{this.state.titleData}</h1>
