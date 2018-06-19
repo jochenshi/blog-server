735e55e00c49dd138853ca21f516911c752ae866
jochenshi <jochenshi@gmail.com>
2018-06-19 17:22:47 +0800

    add some function entry in markdown.js

--- static/src/component/markdown/markdown.js
+++ static/src/component/markdown/markdown.js
@@ -19,17 +19,25 @@ marked.setOptions({
     smartypants: false
 });
 
+
+/*
+* 对于按钮的事件，通过props的方式传入
+* uploadPicture: function 上传图片
+* handleSave: function  保存文章
+* releasePaper: function  发布文章
+* */
+
 class Markdown extends Component{
     constructor(props) {
         super(props);
         this.state = {
-            titleData: "",
-            htmlData: ""
+            titleData: "ad",
+            htmlData: "asd"
         };
         this.icons = [
-            {class: "icon-picture", title: "图片"},
-            {class: "icon-save", title: "保存"},
-            {class: "icon-paper", title: "发布文章"}
+            {class: "icon-picture", title: "图片", key: "picture", click: this.uploadPicture},
+            {class: "icon-save", title: "保存", key: "save", click: this.handleSave},
+            {class: "icon-paper", title: "发布文章", key: "release-paper", click: this.releasePaper}
         ];
     }
 
@@ -53,6 +61,37 @@ class Markdown extends Component{
             })
         }, 2000)
     };
+
+    //处理点击保存按钮以及触发保存操作时的事件
+    handleSave() {
+        this.handleClick("handleSave", this, this.state.titleData, this.state.htmlData);
+    }
+
+    //处理添加图片的事件
+    uploadPicture() {
+        if (typeof this.props.uploadPicture === "function") {
+            console.log("this can trigger save function");
+            this.props.uploadPicture.call(this);
+        } else {
+            console.error("uploadPicture is not a function or does not exist")
+        }
+    }
+
+    //处理发布文章的事件
+    releasePaper() {
+        this.handleClick("releasePaper", this, this.state.titleData, this.state.htmlData);
+    }
+
+    //用于绑定事件的相关操作
+    handleClick(fun, thisArg, ...args) {
+        if (typeof thisArg.props[fun] === "function") {
+            console.log("this can trigger click function");
+            thisArg.props[fun].call(thisArg, ...args)
+        } else {
+            console.error(fun + " is not a function or does not exist");
+            console.log(...args)
+        }
+    }
 
     //处理按键按下的事件，在此处是为了实现Tab缩进的效果
     handleKeyDown = (e) => {
@@ -83,7 +122,11 @@ class Markdown extends Component{
                         {
                             this.icons.length ?
                                 this.icons.map((val, index) => {
-                                    return <li key={index} className={"iconfont " + val.class} title={val.title}></li>
+                                    return <li key={val.key}
+                                               onClick={val.click ? val.click.bind(this) : () => {console.log("no function called in " + val.key)}}
+                                               className={"iconfont " + val.class}
+                                               title={val.title}>
+                                    </li>
                                 }) :
                                 ""
                         }
@@ -95,7 +138,8 @@ class Markdown extends Component{
                 </div>
                 <div className={"display-area"}>
                     <h1 className={"display-title"}>{this.state.titleData}</h1>
-                    <div  className={"display-body"} dangerouslySetInnerHTML={{__html: this.state.htmlData}}></div>
+                    <div className={"display-body"} dangerouslySetInnerHTML={{__html: this.state.htmlData}}>
+                    </div>
                 </div>
             </div>
         )
